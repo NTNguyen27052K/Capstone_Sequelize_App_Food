76036358a8c039f431bb49c03b6b93b0837748af
NTNguyen27052K <ntnguyen275@gmail.com>
2023-08-30 23:05:24 +0700

    4th

--- src/Controllers/likeController.js
+++ src/Controllers/likeController.js
@@ -1,13 +1,14 @@
 
+import { where } from 'sequelize';
 import sequelize from '../Model/index.js';
 import initModels from "../Model/init-models.js";
 
 const model = initModels(sequelize);
 const getLike = async (request, response) => {
   //SELECT * FROM user
-  let data = await model.like_res.findAll();
-  // let data = await User.findOne({where: {full_name: "John wick"}});
-  // response send data to client
+  let data = await model.like_res.findAll({
+  include:['user', 're']});
+  
   response.send(data);
 };
 
